b80618ad80ce0174c4b748e442ebde9756124753
Jarno <22636106+Jarno908@users.noreply.github.com>
2022-04-29 20:02:51 +0000

    Added styling to signIn page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,17 +1,30 @@
+import { Button, Center, Container, Stack, Text, Title } from "@mantine/core"
 import { Provider } from "next-auth/providers"
 import { getProviders, signIn } from "next-auth/react"
 
 export default function SignIn({ providers }: { providers: Provider }) {
   return (
-    <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id, null, { prompt: "login" })}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
-    </>
+    <Container sx={(theme) => ({ paddingTop: theme.spacing.xl })}>
+      <Center>
+        <Stack>
+          <Title order={1} align="center" >Login</Title>
+          <Text>Log in met een van de onderstaande methodes:</Text>
+        </Stack>
+      </Center>
+      <Center sx={(theme) => ({ marginTop: theme.spacing.md })}>
+        <Stack sx={(theme) => ({ 
+          padding: theme.spacing.xl,
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[2],
+          borderRadius: theme.radius.md
+          })}>
+          {Object.values(providers).map((provider) => (
+            <Button key={provider.name} onClick={() => signIn(provider.id, null, { prompt: "login" })}>
+              Log in met {provider.name}
+            </Button>
+          ))}
+        </Stack>
+      </Center>
+    </Container>
   )
 }
 
